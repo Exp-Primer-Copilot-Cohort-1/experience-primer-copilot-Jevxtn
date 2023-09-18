d0623c74c40b2b38576979c1814aab78a00df5bd
John Emmanuel Villegas <144324850+Jevxtn@users.noreply.github.com>
2023-09-18 13:51:41 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
-function skillMember() {
-    console.log('skill member')
+function skillsMember() {
+    var skills = document.querySelectorAll('.skills');
+    skills.forEach(function (skill) {
+        var percentage = skill.getAttribute('data-percentage');
+        var progressBar = skill.querySelector('.progress-bar');
+        progressBar.style.width = percentage + '%';
+    });
 }
